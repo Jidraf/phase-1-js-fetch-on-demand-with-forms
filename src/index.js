f19ea544755866548e827bd74d31bd63dc74c13a
Jidraf Kimachia <raphjid170@gmail.com>
2022-06-17 20:17:19 +0300

    phase-1-js-fetch-on-demand-with-forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+    const input = document.querySelector('form');
+    input.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+    
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        title.innerText = data.title;
+        sum.innerText = data.summary;
+      })
+      .catch( err => console.log(err))
 
+
+
+    })
+}
+init()
 document.addEventListener('DOMContentLoaded', init);
